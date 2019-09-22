3359639441015405c5c47473602c1917013c223f
Raihan Seraj <raihan.seraj@mail.mcgill.caa>
2019-09-22 14:03:06 -0400

    need to put a wrapper around Constrained-K-means

--- classification.py
+++ classification.py
@@ -2,8 +2,10 @@ from sklearn.cluster import *
 # from pyclustering.cluster.xmeans import xmeans 
 # from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from classifiers import x_means
+from k_means_constrained import KMeansConstrained
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import adjusted_rand_score
+
 
 
 
@@ -56,7 +58,7 @@ def classify(X_train, y_train,configs):
 
 		clf = x_means.X_means(min_num_clusters,max_num_clusters)
 
-		
+
 		
 
 		accuracy_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='accuracy')
@@ -79,8 +81,36 @@ def classify(X_train, y_train,configs):
 
 
 
-	if configs['algorithm']['Constrained_K-means']:
-		pass ## define functions for constrained K-means from pyclustering
+	if configs['algorithm']['name']=="Constrained_K-means":
+
+		min_num_clusters = configs['algorithm']['minimum number of clusters']
+
+		max_num_clusters = configs['algorithm']['maximum number of clusters']
+
+
+
+		clf = KMeansConstrained(n_clusters=min_num_clusters, size_min=min_num_clusters, size_max=max_num_clusters)
+
+		accuracy_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='accuracy')
+
+		rand_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='adjusted_rand_score')
+
+		homogeneity_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='homogeneity_score')
+
+		mutual_info_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='mutual_info_score')
+
+		completeness_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='completeness_score')
+
+		if configs['classification type']=="Binary":
+
+			f1_scores = cross_val_score(clf, X_train, y_train, cv=configs['preprocessing']['Cross_Validation_Fold'],scoring='f1_macro')
+
+			return accuracy_scores, rand_scores, homogeneity_scores, mutual_info_scores, completeness_scores,f1_scores
+		else:
+			return accuracy_scores, rand_scores, homogeneity_scores, mutual_info_scores, completeness_scores
+
+
+
 	
 	if configs['algorithm']['K-Prototype']:
 		pass
