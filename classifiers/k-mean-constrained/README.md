8dc161b8051fc87d025c773e9b6f23795732c808
Raihan Seraj <raihan.seraj@mail.mcgill.caa>
2019-10-31 22:22:34 -0400

    Added UNSW data a

---
+++ classifiers/k-mean-constrained/README.md
@@ -0,0 +1,62 @@
+# k-means-constrained
+K-means clustering implementation whereby a minimum and/or maximum size for each
+cluster can be specified.
+
+This K-means implementation modifies the cluster assignment step (E in EM)
+by formulating it as a Minimum Cost Flow (MCF) linear network
+optimisation problem. This is then solved using a cost-scaling
+push-relabel algorithm and uses [Google's Operations Research tools's
+`SimpleMinCostFlow`](https://developers.google.com/optimization/flow/mincostflow)
+which is a fast C++ implementation.
+
+This package is inspired by [Bradley et al.](https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/tr-2000-65.pdf).
+The original Minimum Cost Flow (MCF) network proposed by Bradley et al.
+has been modified so maximum cluster sizes can also be specified along
+with minimum cluster size. 
+
+The code is based on [scikit-lean's `KMeans`](http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html)
+and implements the same API with modifications.
+
+Ref:
+1. [Bradley, P. S., K. P. Bennett, and Ayhan Demiriz. "Constrained k-means clustering."
+    Microsoft Research, Redmond (2000): 1-8.](https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/tr-2000-65.pdf)
+2. [Google's SimpleMinCostFlow C++ implementation](https://github.com/google/or-tools/blob/master/ortools/graph/min_cost_flow.h)
+
+# Example
+```python
+>>> from k_means_constrained import KMeansConstrained
+>>> import numpy as np
+>>> X = np.array([[1, 2], [1, 4], [1, 0],
+>>>                [4, 2], [4, 4], [4, 0]])
+>>> clf = KMeansConstrained(
+>>>     n_clusters=2,
+>>>     size_min=2,
+>>>     size_max=5,
+>>>     random_state=0
+>>> )
+>>> clf.fit(X)
+array([0, 0, 0, 1, 1, 1], dtype=int32)
+>>> clf.cluster_centers_
+array([[ 1.,  2.],
+       [ 4.,  2.]])
+>>> clf.predict([[0, 0], [4, 4]])
+array([0, 1], dtype=int32)
+```
+
+# Installation
+Requires [Google's OR-tools to be installed](https://developers.google.com/optimization/introduction/installing/binary).
+
+Currently tested with:
+* scikit-learn == 0.19.1
+* ortools == 6.7.4973
+
+As this package uses internal scikit learn methods associated with k-means
+it may break with other versions of scikit-learn. This can be addressed
+in the future by importing the internal methods into this project.
+
+Added support with sklearn >=0.20
+
+# TODO:
+- [ ] Documentation
+- [ ] Test with sparse `X`
+- [ ] Remove dependencies on internal scikit learn methods by importing them into this project
--- preprocessing.py
+++ preprocessing.py
@@ -7,12 +7,45 @@ import io
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+import category_encoders as ce
+import glob
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
 def preprocess(data_url, dataname,configs): 
 	s=requests.get(data_url).content
 	
 	if dataname=="KDD_10_percent":
 		
 		data = pd.read_csv('./Kdd_Data/kddcup.data_10_percent')
+	elif dataname=="UNSW":
+		file_location=glob.glob("./UNSW_data/unsw_data.csv")
+		li=[]
+		for filenames in file_location:
+			data = pd.read_csv(filenames, low_memory=False) 
+
+			# dtypes={"srcip": object, "sport": int, "dstip": object, "dsport": int, "proto": object, "state": object, 
+			# "dur": float, "sbytes": int, "dbytes": int, "sttl": int, "dttl": int, "sloss": int, "dloss": int, "service": object, 
+			# "Sload": float, "Dload": float, "Spkts": int, "Dpkts": int, "swin": int, "dwin": int, "stcpb": int, 
+			# "dtcpb": int, "smeansz": int, "dmeansz": int, "trans_depth": int, "res_bdy_len": int, "Sjit": float,
+			# "Djit": float, "Sintpkt": float, "Dintpkt": float, "tcprtt": float, "synack": float, "ackdat": float,
+			# "is_sm_ips_ports": int, "ct_state_ttl": int, "ct_flw_http_mthd": int, "is_ftp_login": int, "ct_ftp_cmd": int, 
+			# "ct_srv_src": int, "ct_srv_dst": int, "ct_dst_ltm": int, "ct_src_ltm": int, "ct_src_dport_ltm": int, 
+			# "ct_dst_sport_ltm": int, "ct_dst_src_ltm": int, "attack_cat": object, "Label": int, "Stime": int, "Ltime": int }
+
+			# df.columns=['srcip', 'sport', 'dstip', 'dsport', 'proto', 'state', 'dur', 'sbytes', 'dbytes', 'sttl', 'dttl', 'sloss',
+			# 'dloss', 'service', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'trans_depth',
+			# 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime', 'Sintpkt', 'Dintpkt', 'tcprtt', 'synack', 'ackdat', 'is_sm_ips_ports', 'ct_state_ttl',
+			# 'ct_flw_http_mthd', 'is_ftp_login', 'ct_ftp_cmd', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm',
+			# 'ct_dst_src_ltm', 'attack_cat', 'Label']
+
+			# df.astype(dtypes)
+
+		
+		#import ipdb;ipdb.set_trace()
 	else:
 
 		data = pd.read_csv(io.StringIO(s.decode('utf-8')))
@@ -203,9 +236,40 @@ def preprocess(data_url, dataname,configs):
 		else:
 			X_train = features
 
+	if dataname=="UNSW":
 		
+		dataset=clean_dataset(data)
+		#import ipdb;ipdb.set_trace()
+		y_train=np.array(dataset.iloc[:,-1])
 		
+		# import ipdb;ipdb.set_trace()
+		# for i in range(features.shape[1]):
+		# 	features.iloc[:,i]=pd.to_numeric(features.iloc[:,i], errors='coerce')
+		# import ipdb;ipdb.set_trace()
+		features= np.array(dataset.iloc[:,0:-1])
 		
+		if configs['preprocessing']['StandardScaler']:
+			from sklearn.preprocessing import StandardScaler
+			print("Using StandardScaler")
+		
+			scaler = StandardScaler()
+		
+			scaled_features = scaler.fit_transform(features)
+
+			X_train = scaled_features
+		elif configs['preprocessing']['MinMaxScaler']:
+			from sklearn.preprocessing import MinMaxScaler
+			print("Using MinMax Scaler")
+			
+			scaler=MinMaxScaler(feature_range=(0,1))
+			
+			scaled_features = scaler.fit_transform(features)
+
+			X_train = scaled_features
+		else:
+			X_train = features
+
+
 	return X_train, y_train.reshape(-1,) 
 
 
