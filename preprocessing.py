2afdc19f66473c5689340fc1bbd201acc359a01f
Raihan Seraj <raihan.seraj@mail.mcgill.caa>
2019-10-31 22:24:07 -0400

    deleted commented files from preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -27,25 +27,6 @@ def preprocess(data_url, dataname,configs):
 		for filenames in file_location:
 			data = pd.read_csv(filenames, low_memory=False) 
 
-			# dtypes={"srcip": object, "sport": int, "dstip": object, "dsport": int, "proto": object, "state": object, 
-			# "dur": float, "sbytes": int, "dbytes": int, "sttl": int, "dttl": int, "sloss": int, "dloss": int, "service": object, 
-			# "Sload": float, "Dload": float, "Spkts": int, "Dpkts": int, "swin": int, "dwin": int, "stcpb": int, 
-			# "dtcpb": int, "smeansz": int, "dmeansz": int, "trans_depth": int, "res_bdy_len": int, "Sjit": float,
-			# "Djit": float, "Sintpkt": float, "Dintpkt": float, "tcprtt": float, "synack": float, "ackdat": float,
-			# "is_sm_ips_ports": int, "ct_state_ttl": int, "ct_flw_http_mthd": int, "is_ftp_login": int, "ct_ftp_cmd": int, 
-			# "ct_srv_src": int, "ct_srv_dst": int, "ct_dst_ltm": int, "ct_src_ltm": int, "ct_src_dport_ltm": int, 
-			# "ct_dst_sport_ltm": int, "ct_dst_src_ltm": int, "attack_cat": object, "Label": int, "Stime": int, "Ltime": int }
-
-			# df.columns=['srcip', 'sport', 'dstip', 'dsport', 'proto', 'state', 'dur', 'sbytes', 'dbytes', 'sttl', 'dttl', 'sloss',
-			# 'dloss', 'service', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'trans_depth',
-			# 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime', 'Sintpkt', 'Dintpkt', 'tcprtt', 'synack', 'ackdat', 'is_sm_ips_ports', 'ct_state_ttl',
-			# 'ct_flw_http_mthd', 'is_ftp_login', 'ct_ftp_cmd', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_src_ ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm',
-			# 'ct_dst_src_ltm', 'attack_cat', 'Label']
-
-			# df.astype(dtypes)
-
-		
-		#import ipdb;ipdb.set_trace()
 	else:
 
 		data = pd.read_csv(io.StringIO(s.decode('utf-8')))
@@ -239,13 +220,10 @@ def preprocess(data_url, dataname,configs):
 	if dataname=="UNSW":
 		
 		dataset=clean_dataset(data)
-		#import ipdb;ipdb.set_trace()
+		
 		y_train=np.array(dataset.iloc[:,-1])
 		
-		# import ipdb;ipdb.set_trace()
-		# for i in range(features.shape[1]):
-		# 	features.iloc[:,i]=pd.to_numeric(features.iloc[:,i], errors='coerce')
-		# import ipdb;ipdb.set_trace()
+		
 		features= np.array(dataset.iloc[:,0:-1])
 		
 		if configs['preprocessing']['StandardScaler']:
